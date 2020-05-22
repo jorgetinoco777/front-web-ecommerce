4ba1eef3324d69e749c18f180ff1b29c0718b773
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2020-05-21 23:31:16 -0500

    Agregar pantallas a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreRegisterComponent } from './pages/store-register/store-register.component';
+import { RoundsmanRegisterComponent } from './pages/roundsman-register/roundsman-register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreRegisterComponent,
+    RoundsmanRegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
