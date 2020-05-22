d35146eb36e970078b66cf3087e7b70fcff4ed00
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2020-05-21 23:29:14 -0500

    Pagina para registro de motorizados

---
+++ src/app/pages/roundsman-register/roundsman-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roundsman-register',
+  templateUrl: './roundsman-register.component.html',
+  styleUrls: ['./roundsman-register.component.css']
+})
+export class RoundsmanRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
