56e048855becb5545cc566f72df013e63d100403
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2020-05-22 02:13:59 -0500

    Formulario para registro de negocios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDs4l-ci5DluSEcJqhTxKWLXiAnuf2X1Eg",
+    authDomain: "alpaso-e0fc9.firebaseapp.com",
+    databaseURL: "https://alpaso-e0fc9.firebaseio.com",
+    projectId: "alpaso-e0fc9",
+    storageBucket: "alpaso-e0fc9.appspot.com",
+    messagingSenderId: "292927541673",
+    appId: "1:292927541673:web:dc9839da724c9527a0f4b8"
+  }
 };
 
 /*
