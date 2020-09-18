4d6f169a041ad259e78576c6435045daff084827
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2020-09-17 20:07:35 -0500

    Servicio para crear comercios

--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoresService {
 
-  constructor() { }
+  name_function: string;
+  url: string;
+  headers = new HttpHeaders();
+
+  constructor( private http: HttpClient ) {
+    this.headers.set("Content-Type", "application/json");
+    this.url = `${ environment.firebaseConfig.databaseURL }`;
+  }
+
+  save( data ) {
+    this.name_function = `stores-register.json`;
+    return this.http.post(`${ this.url }/${ this.name_function }`, { headers: this.headers, ...data });
+  }
+
 }
