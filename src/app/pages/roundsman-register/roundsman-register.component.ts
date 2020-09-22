254d1bf6313f81dd8cb452d844b8386b1fdeec09
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2020-09-22 17:32:11 -0500

    Formulario de registro de repartidores

--- src/app/pages/roundsman-register/roundsman-register.component.ts
+++ src/app/pages/roundsman-register/roundsman-register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RoundsmanService } from '../../services/roundsman.service';
 import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-roundsman-register',
@@ -10,54 +11,75 @@ import Swal from 'sweetalert2';
 })
 export class RoundsmanRegisterComponent implements OnInit {
 
+  formRoundsman: FormGroup;
   roundsman: any;
-  roundsmanForm: FormGroup;
+
+  status: string;
+  date: string;
+  city: string;
 
   constructor( private _formBuilder: FormBuilder,
                private _roundsmanService: RoundsmanService ) { }
 
   ngOnInit(): void {
 
-    this.roundsmanForm = this._formBuilder.group({
-      id           : ['', Validators.required],
-      name         : ['', Validators.required],
-      phone        : ['', Validators.required],
-      email        : ['', Validators.required, Validators.email],
-      type         : ['', Validators.required],
-      message      : ['', Validators.required],
-      terms_accept : [false, Validators.required]
+    this.status = 'PENDIENTE';
+
+    this.formRoundsman = this._formBuilder.group({
+      name: [ 'Jorge', [ Validators.required, Validators.minLength(3)] ],
+      phone: [ '0967647864', [ Validators.required, Validators.minLength(10), Validators.maxLength(10)] ],
+      email: [ 'jorge@hotmail.com', [ Validators.required, Validators.email ] ],
+      type: [ 'Moto', Validators.required ],
+      message: [ 'Hola, me gustaría formar parte del equipo Al Paso', [ Validators.required, Validators.minLength(3) ] ],
+      terms_accept: [ false ]
     });
+
+      console.log("Forms: ", this.formRoundsman);
+      
 
   }
 
   send() {
 
-    let id = this.roundsmanForm.get('id').value;
-    let name = this.roundsmanForm.get('name').value;
-    let phone = this.roundsmanForm.get('phone').value;
-    let email = this.roundsmanForm.get('email').value;
-    let type = this.roundsmanForm.get('type').value;
-    let message = this.roundsmanForm.get('message').value;
-    let terms_accept = this.roundsmanForm.get('terms_accept').value;
+    if ( !this.formRoundsman.get('terms_accept').value ) {
+      Swal.fire('¿Deseas continuar?', 'Por favor, acepta los términos y condiciones.', 'error'
+      )
+      return;
+    }
 
     this.roundsman = {
-      id,
-      name,
-      phone,
-      email,
-      type,
-      message,
-      terms_accept
+      name: this.formRoundsman.get('name').value,
+      phone: this.formRoundsman.get('phone').value,
+      email: this.formRoundsman.get('email').value,
+      type: this.formRoundsman.get('type').value,
+      message: this.formRoundsman.get('message').value,
+      terms_accep: this.formRoundsman.get('terms_accept').value,
+      status: this.status,
+      date: this.date
     };
 
+    console.log("Roundsman: ", this.roundsman);
+
+    this.date = new Date().toLocaleString('en-ES', {
+      timeZone: 'America/Guayaquil',
+      hour12: false
+    });
+
     this._roundsmanService.save( this.roundsman ).subscribe( ( confirm ) => {
-      console.log("Confirmación", confirm);
       Swal.fire({
         icon: 'success',
         title: 'Listo...',
-        text: 'Registro Exitos!',
-        footer: 'Muy pronto nuestro un representante de Al Paso se pondrá en contacto contigo.'
-      })
+        text: 'Registro Exitoso!',
+        footer: '<span class="text-secondary">Muy pronto nos pondremos en contacto contigo.</span>'
+      });
+
+      this.formRoundsman.get('name').setValue('');
+      this.formRoundsman.get('phone').setValue('');
+      this.formRoundsman.get('email').setValue('');
+      this.formRoundsman.get('type').setValue('');
+      this.formRoundsman.get('message').setValue('');
+      this.formRoundsman.get('terms_accept').setValue(false);
+      
     });
 
   }
