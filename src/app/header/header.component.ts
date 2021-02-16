7b4e005ce6b51e0c6af6e1189fe8ed6349da10a6
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2021-02-16 12:17:03 -0500

    Transacion para navbar corregido

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,6 +12,43 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // MAIN MENU TOGGLER ICON (MOBILE SITE ONLY)
+    $('[data-toggle="navbarToggler"]').click(function () {
+      $('.navbar').toggleClass('active');
+      $('body').toggleClass('canvas-open');
+    });
+    // MAIN MENU TOGGLER ICON
+    $('.navbar-toggler').click(function () {
+        $('.navbar-toggler-icon').toggleClass('active');
+    });
+
+    // NAVBAR STICKY
+    var $stickyNav = $(".navbar-sticky");
+
+    $(window).on("scroll load", function () {
+      var scroll = $(window).scrollTop();
+      //console.log("Scroll: ", scroll);
+      if (scroll >= 120) {
+          $stickyNav.addClass("navbar-sticky-moved-up");
+      } else {
+          $stickyNav.removeClass("navbar-sticky-moved-up");
+      }
+      // apply transition
+      if (scroll >= 250) {
+          $stickyNav.addClass("navbar-sticky-transitioned");
+      } else {
+          $stickyNav.removeClass("navbar-sticky-transitioned");
+      }
+      // sticky on
+      if (scroll >= 500) {
+          $stickyNav.addClass("navbar-sticky-on");
+      } else {
+          $stickyNav.removeClass("navbar-sticky-on");
+      }
+
+    });
+
   }
 
 }
--- src/assets/js/active.js
+++ src/assets/js/active.js
@@ -33,12 +33,28 @@
 
     // :: 2.0 NAVIGATION MENU ACTIVE CODE
     // dropdown for mobile
-    $(document).ready(function () {
+    $(document).on(function () {
         checkWidth(true);
         $(window).resize(function () {
             checkWidth(false);
         });
     });
+
+    // Navbar JS
+	$(window).on('scroll',function() {
+		if ($(this).scrollTop()>200){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		}
+	});
+	
+	$(document).on('click','.navbar-collapse.show',function(e){
+		if( $(e.target).is('a') && $(e.target).attr('class')!= 'dropdown-toggle' ){
+			$(this).collapse('hide');
+		}
+	});	
 
     function checkWidth(init) {
         // If browser resized, check width again 
@@ -50,44 +66,6 @@
             });
         }
     }
-    function navMenu() {
-
-        // MAIN MENU TOGGLER ICON (MOBILE SITE ONLY)
-        $('[data-toggle="navbarToggler"]').click(function () {
-            $('.navbar').toggleClass('active');
-            $('body').toggleClass('canvas-open');
-        });
-        // MAIN MENU TOGGLER ICON
-        $('.navbar-toggler').click(function () {
-            $('.navbar-toggler-icon').toggleClass('active');
-        });
-
-        // NAVBAR STICKY
-        var $stickyNav = $(".navbar-sticky");
-
-        $(window).on("scroll load", function () {
-            var scroll = $(window).scrollTop();
-            if (scroll >= 120) {
-                $stickyNav.addClass("navbar-sticky-moved-up");
-            } else {
-                $stickyNav.removeClass("navbar-sticky-moved-up");
-            }
-            // apply transition
-            if (scroll >= 250) {
-                $stickyNav.addClass("navbar-sticky-transitioned");
-            } else {
-                $stickyNav.removeClass("navbar-sticky-transitioned");
-            }
-            // sticky on
-            if (scroll >= 500) {
-                $stickyNav.addClass("navbar-sticky-on");
-            } else {
-                $stickyNav.removeClass("navbar-sticky-on");
-            }
-
-        });
-    }
-    navMenu();
 
     // :: 3.0 SCROLL TO TOP ACTIVE CODE
     var offset = 300;
