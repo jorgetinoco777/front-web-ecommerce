e4d4dede7d0f0abd1eaab098848d7ba31878d023
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2021-02-16 16:02:42 -0500

    Funcionalidad para carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var $:any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +13,16 @@ export class HomeComponent implements OnInit {
   constructor( private router: Router ) { }
 
   ngOnInit(): void {
+    $('.features-slider.owl-carousel').owlCarousel({
+      items: 1,
+      loop: true,
+      nav: false,
+      dots: true,
+      smartSpeed: 1000,
+      autoplay: true,
+      autoplayTimeout: 6000,
+      dotsContainer: '.features-content'
+    });
   }
 
   goToRegisterBussiness() {
