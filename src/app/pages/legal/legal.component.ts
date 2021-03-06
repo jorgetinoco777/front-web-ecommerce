4d6eac921dcf6bfc3c729a0336f93b26fd8b7312
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2021-03-05 22:21:02 -0500

    Agregar google analitycs a Legal terminos y condiciones

--- src/app/pages/legal/legal.component.ts
+++ src/app/pages/legal/legal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+ // Firebase
+import { AngularFireAnalytics } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-legal',
@@ -7,7 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LegalComponent implements OnInit {
 
-  constructor() { }
+  constructor( private analytics: AngularFireAnalytics,
+               private router: Router ) {
+
+    // Guardar registro Google Anatytics
+    this.analytics.logEvent('page_view', {
+      page_location: this.router.url,
+      page_path: this.router.url,
+      page_title: 'Legal Terminos y condiciones'
+    });
+
+  }
 
   ngOnInit(): void {
   }
