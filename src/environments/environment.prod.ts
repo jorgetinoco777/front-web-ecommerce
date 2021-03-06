0451702332ab02b150ef78f16111b6376c1e1a03
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2021-03-05 19:47:08 -0500

    Agregar configuracion firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDs4l-ci5DluSEcJqhTxKWLXiAnuf2X1Eg",
+    authDomain: "alpaso-e0fc9.firebaseapp.com",
+    databaseURL: "https://alpaso-e0fc9.firebaseio.com",
+    projectId: "alpaso-e0fc9",
+    storageBucket: "alpaso-e0fc9.appspot.com",
+    messagingSenderId: "292927541673",
+    appId: "1:292927541673:web:77d28455286f04a4a0f4b8",
+    measurementId: "G-6H6CDDDZ7H"
+  }
 };
+
+
