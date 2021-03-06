5b4683e1a6b9e32f7550da2ae02aa92f6761f302
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2021-03-05 22:27:15 -0500

    Agregar google analitycs Acerca de nosotros

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAnalytics } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor( private analytics: AngularFireAnalytics,
+               private router: Router )
+  {
+
+    // Guardar registro Google Anatytics
+    this.analytics.logEvent('page_view', {
+      page_location: this.router.url,
+      page_path: this.router.url,
+      page_title: 'Acerca de Al Paso Delivery'
+    });
+
+  }
 
   ngOnInit(): void {
   }
