c85f9b40cfe8d77a1b106d9a0962c28b97ec6704
Jorge Tinoco <jorgeluistinocoj@gmail.com>
2021-03-05 22:16:31 -0500

    Agregar google analitycs routing al paso app

--- src/app/pages/landing-store/landing-store.component.ts
+++ src/app/pages/landing-store/landing-store.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { DeviceDetectorService } from 'ngx-device-detector';
+// import { DeviceDetectorService } from 'ngx-device-detector';
+import { AngularFireAnalytics } from '@angular/fire/analytics';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-landing-store',
@@ -14,44 +16,26 @@ export class LandingStoreComponent implements OnInit {
   appStore: string;
   playStore: string;
 
-  constructor( private deviceService: DeviceDetectorService,
-               private router: Router ) {
+  constructor( private analytics: AngularFireAnalytics,
+               // private deviceService: DeviceDetectorService,
+               private router: Router,
+               private _routingAppService: UtilsService )
+  {
+
+    // Guardar registro Google Anatytics
+    this.analytics.logEvent('page_view', {
+      page_location: this.router.url,
+      page_path: this.router.url,
+      page_title: 'Routing App Al Paso'
+    });
+
     this.appStore = 'https://apps.apple.com/us/app/id1522625554';
     this.playStore = 'https://play.google.com/store/apps/details?id=app.gocomunicacion.alpaso';
+
   }
 
   ngOnInit(): void {
-    this.epicFunction();
-
-  }
-
-  epicFunction(): void {
-
-    this.deviceInfo = this.deviceService.getDeviceInfo();
-
-    const isMobile = this.deviceService.isMobile();
-    const isTablet = this.deviceService.isTablet();
-    const isDesktopDevice = this.deviceService.isDesktop();
-
-    /*
-    console.log(this.deviceInfo);
-    console.log(isMobile);  // returns if the device is a mobile device (android / iPhone / windows-phone etc)
-    console.log(isTablet);  // returns if the device us a tablet (iPad etc)
-    console.log(isDesktopDevice); // returns if the app is running on a Desktop browser.
-    */
-
-    // Validar si es mobile or tablet
-    if ( isMobile || isTablet ) {
-      if ( this.deviceInfo.os === 'iOS' ) {
-        window.location.href = this.appStore;
-      } else if ( this.deviceInfo.os === 'Android' ) {
-        window.location.href = this.playStore;
-      } else {
-        this.router.navigate(['/home']);
-      }
-    } else {
-      this.router.navigate(['/home']);
-    }
+    this._routingAppService.routing( this.appStore, this.playStore, '/home');
 
   }
 
